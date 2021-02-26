f33ef48b1281d0b16dce42109d37b49b89e9fa62
aalshaiba <abdullah.alshaiba@gmail.com>
2021-02-27 00:59:32 +0400

    first commit

--- mysite/islam/prayers.py
+++ mysite/islam/prayers.py
@@ -2,17 +2,14 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
+
 def get_prayers():
     url = 'https://m.awqaf.ae/prayertimes.aspx'
-
     response = requests.get(url)
-
     soup = BeautifulSoup(response.text, 'lxml')
-
     content = []
     for row in soup.find_all('span'):
         content.append(row.text)
-
     prayers = content[5:-2]
     times = []
     names = prayers[0::2]
@@ -20,5 +17,4 @@ def get_prayers():
         time = re.match('\d\d:\d\d', x)
         if time is not None:
             times.append(time.group(0))
-
     return zip(names, times)
