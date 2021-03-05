731fd12aa254275e3aad671c8da65fc0c7a7cb92
aalshaiba <abdullah.alshaiba@gmail.com>
2021-03-05 14:07:01 +0400

    first commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -125,5 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static/')
+
